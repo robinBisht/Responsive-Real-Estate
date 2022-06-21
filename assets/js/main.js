2cbb1e0e720c74f543c3a5198aa13bc161f944f4
robinBisht <robinbsiht7@gmail.com>
2022-06-21 22:32:22 +0530

    popular section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,17 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 /*=============== SWIPER POPULAR ===============*/
 
-
+let swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:'auto',
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 /*=============== VALUE ACCORDION ===============*/
 
 
